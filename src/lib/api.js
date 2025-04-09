5d776831f70d95c55e684e7fde018236571eedf6
dinethprasanna <dinethprasanna58@gmail.com>
2025-04-09 11:59:31 +0530

    .ENV BE url added env var

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 //here we have the rtk queries
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = process.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const api = createApi({
     reducerPath: "api",
